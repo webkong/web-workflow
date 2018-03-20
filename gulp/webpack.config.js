7985b81cf1061efbfd599859326016195edb28db
webkong <772930508@qq.com>
2018-03-20 18:52:34 +0800

    fix

--- gulp/webpack.config.js
+++ gulp/webpack.config.js
@@ -1,5 +1,7 @@
 /**
  * Created by wangsw on 08/05/2016.
+ * Modify by wangsw on 2018年03月05日11:22:44
+ * use webpack4.1.0
  * @version 1.0.4
  */
 let _ = require('lodash');
@@ -106,7 +108,7 @@ let getAlias = function(env) {
 
 // 插件配置
 let getPlugins = function(env) {
-  //html 
+  //html
   let htmlfiles = glob.sync(config.src + '/html/*.html');
   let htmlArr = []; //存储文件path数组
   let newHtmlArr = []; //对象数组
@@ -119,7 +121,7 @@ let getPlugins = function(env) {
           filename: config.dest + '/' + name + '.html',
           template: f,
           inject: true,
-          chunks:['common',name],
+          chunks: ['common', name],
           minify: {
             removeComments: true,
             collapseWhitespace: true,
@@ -134,7 +136,7 @@ let getPlugins = function(env) {
           filename: config.dest + '/' + name + '.html',
           template: f,
           inject: true,
-          chunks:['common',name]
+          chunks: ['common', name]
         })
       );
     }
@@ -146,10 +148,7 @@ let getPlugins = function(env) {
       $: jsLib
     }),
     // 抽离公共模块
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common',
-      filename: 'common.js'
-    }),
+
     new ExtractTextPlugin({
       filename: '../css/[name].css'
     }),
@@ -168,7 +167,7 @@ let getPlugins = function(env) {
 
   if (env === 'production') {
     // 线上模式的配置，去除依赖中重复的插件/压缩js/排除报错的插件
-    plugins = _.union(defaultPlugins,newHtmlArr, [
+    plugins = _.union(defaultPlugins, newHtmlArr, [
       new webpack.NoEmitOnErrorsPlugin(),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
@@ -179,12 +178,13 @@ let getPlugins = function(env) {
       })
     ]);
   } else {
-    plugins = _.union(defaultPlugins,newHtmlArr, []);
+    plugins = _.union(defaultPlugins, newHtmlArr, []);
   }
 
   return plugins;
 };
 // 作为函数导出配置，代码更简洁
+console.log(newEntries);
 module.exports = function(env) {
   return {
     //entry: config.jsSrc + '/index.js',
@@ -206,6 +206,20 @@ module.exports = function(env) {
     cache: true,
     module: {
       rules: getLoaders(env)
+    },
+    optimization: {
+      runtimeChunk: {
+        name: 'manifest'
+      },
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'common',
+            chunks: 'all'
+          }
+        }
+      }
     },
     resolve: {
       extensions: ['.js', '.json', '.jsx', '.css', '.scss', '.vue'],
--- src/demo-web/js/index.js
+++ src/demo-web/js/index.js
@@ -1,2 +1,14 @@
-import '../css/index.scss';
-
+import "../css/index.scss";
+import Loading from '../../common/utils/loading/index';
+let loading = new Loading({
+  msg: {
+    title: { con: 'Loading...' },
+    txt: { con: '' }
+  }
+});
+loading.show();
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    loading.hide();
+  }
+};
